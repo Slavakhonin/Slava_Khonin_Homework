23a229f37215cb1d3331900c43a8c1fe2a3e3895
slavakhonin <slavakhonin@gmail.com>
2015-11-17 15:00:54 +0200

    Homework 6

---
+++ src/Homework4_6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by vkhonin on 11/17/2015.
+ * 4.6. Вводим два числа, вывести их суму, если она в диапазоне от 11 до 19.
+ */
+public class Homework4_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter numbers one:");
+        int number_one = scanner.nextInt();
+        System.out.println("Please enter numbers two:");
+        int number_two = scanner.nextInt();
+
+
+        if (number_one + number_two >11 && number_two + number_one <19){
+            System.out.println("Your number addition is in range between 11 and 19 and is: " + (number_one + number_two));
+        }else{
+            System.out.println("Your numbers addition is out of range 11 and 19");
+        }
+    }
+}
