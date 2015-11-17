c53223d3110ad28c83d88968a303d6d60cc788e5
slavakhonin <slavakhonin@gmail.com>
2015-11-17 10:55:19 +0200

    homework 5

---
+++ src/Homework4_5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by vkhonin on 11/17/2015.
+ */
+public class Homework4_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter numbers one:");
+        int number_one = scanner.nextInt();
+        System.out.println("Please enter numbers two:");
+        int number_two = scanner.nextInt();
+
+        if (number_one > number_two)
+        {
+            System.out.println("Substraction is: " + (number_one - number_two));
+        }else if (number_one < number_two){
+            System.out.println("Addition is: " + (number_one + number_two));
+        }else{
+            System.out.println("Your numbers are equal");
+        }
+
+    }
+}
