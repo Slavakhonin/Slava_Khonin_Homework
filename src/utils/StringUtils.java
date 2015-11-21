274f976d6d1d011bcd284779c2ccc377dedac37e
SlavaKhonin <slavakhonin@gmail.com>
2015-11-21 16:52:45 +0200

    Utils

---
+++ src/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * Created by SlavaKhonin on 11/21/2015.
+ */
+public class StringUtils {
+    public static String sayHello (String name){
+
+        String sayHello = "Hello " + name;
+        return sayHello;
+
+        
+    }
+}
