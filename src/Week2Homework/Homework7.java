02fe8a420c6d416efeee4c278809125c464dcd88
slavakhonin <slavakhonin@gmail.com>
2015-11-25 15:09:52 +0200

    Homework week 2

---
+++ src/Week2Homework/Homework7.java
@@ -0,0 +1,24 @@
+package Week2Homework;
+
+import static utils.ArrayUtils.createRandomArray;
+import static utils.ArrayUtils.showArray;
+import static utils.ArrayUtils.sumAllArrayElements;
+
+/**
+ * Created by vkhonin on 11/25/2015.
+ * 7) Вывести в консоль элементы той половины одномерного массива у которой среднее арифметическое максимальное
+ */
+public class Homework7 {
+    public static void main(String[] args) {
+        int[] array1 = createRandomArray(10,100);
+        int[] array2 = createRandomArray(10,100);
+
+        for (int i = 0; i <array1.length ; i++) {
+            array1[i] += array2[2];
+            }
+
+        showArray(array1);
+
+
+    }
+}
